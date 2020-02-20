a36432440350e8349489c20981759102a9a135fa
Kshitiz Gupta <gupta.ks@northeastern.edu>
2020-02-20 13:44:07 -0500

    changed the postgres user to myprojectuser

--- webapp/settings.py
+++ webapp/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'myproject',
-        'USER': 'postgres',
+        'USER': 'myprojectuser',
         'PASSWORD': 'password',
         'HOST': 'localhost',
         'PORT': '',
