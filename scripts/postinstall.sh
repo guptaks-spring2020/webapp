a908a6c0a02220e5ff02f433aeb484a0aadd8247
Kshitiz Gupta <gupta.ks@northeastern.edu>
2020-03-12 01:53:11 -0400

    adding venv

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd /home/ubuntu/
-#python3 -m venv env
-#source env/bin/activate
+python3 -m venv env
+source env/bin/activate
 python3 manage.py makemigrations
 python3 manage.py migrate
