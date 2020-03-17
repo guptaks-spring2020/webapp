d840b221749f4dfc680b50378dbbef79261cc139
Kshitiz Gupta <gupta.ks@northeastern.edu>
2020-03-17 03:08:44 -0400

    test

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -4,5 +4,5 @@ pip3 install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
 
-sudo cp -rf /scripts/amazon-cloudwatch-agent.json /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+sudo cp -rf scripts/amazon-cloudwatch-agent.json /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json -s
